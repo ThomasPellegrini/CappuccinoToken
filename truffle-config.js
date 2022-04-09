8dbd1f5e927701ba3d00965e8f9fc25130c653bd
ThomasPellegrini <pellegrinithomas1@gmail.com>
2022-04-09 20:12:37 +0200

    Add Token sale on frontend

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+require('dotenv').config({path: './.env'});
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const MetaMaskAccountIndex = 0;
 
 module.exports = {
 // See <http://truffleframework.com/docs/advanced/configuration>
@@ -8,12 +11,18 @@ networks: {
 
     development: {
 
-    port: 7545,
+    port: 8545,
 
     network_id: "*",
 
     host: "127.0.0.1"
-    }
+    },
+     ganache_local: {
+     provider: function() {
+         return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:8545", MetaMaskAccountIndex )
+     },
+     network_id: 1337
+     }
 },
 compilers: {
     solc: {
